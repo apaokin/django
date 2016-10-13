0998986c060fce3b1f0b2a8a00b592766f2ef13b
Paokin <zzz@zzz.com>
2016-10-14 00:53:16 +0300

    Init

---
+++ first/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Player(models.Model):
+	first_name = models.CharField("Имя",max_length=40)
+	last_name = models.CharField("Фамилия",max_length=40)
+	Date_of_reg = models.DateField("Дата регистрации",editable=False,auto_now_add=True)
+
+class Team(models.Model):
+	name = models.CharField("Имя",max_length=40)
+	Date_of_creation = models.DateField("Дата создания",auto_now_add=True)
+#	cap = models.ForeignKey(Player,default=None)
+
+#Player.team = models.ForeignKey(Team)
+#class Match(models.Model):
+#	first_team = models.ForeignKey(Team)
+	#second_team = models.ForeignKey(Team,related_name='+')
